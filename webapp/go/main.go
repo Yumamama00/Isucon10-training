b6a429bd00e2f7f4be38cea4f851c0dcde296b20
YumaIshida00 <yu_basket11@icloud.com>
2022-10-15 16:48:11 +0900

    Sqlx設定変更

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -276,7 +276,8 @@ func main() {
 	if err != nil {
 		e.Logger.Fatalf("DB connection failed : %v", err)
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(151)
+	db.SetMaxIdleConns(151)
 	defer db.Close()
 
 	// Start server
