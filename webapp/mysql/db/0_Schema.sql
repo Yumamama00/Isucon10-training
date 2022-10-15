41ede81ba99bbac5741a70771b3b848fe6035e1f
YumaIshida00 <yu_basket11@icloud.com>
2022-10-15 17:39:56 +0900

    INDEX調整

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE isuumo.estate (
 ALTER TABLE
     isuumo.estate
 ADD
-    INDEX `idx_popularity_id`(popularity DESC, id);
+    INDEX `idx_popularity_id`(popularity, id);
 
 CREATE TABLE isuumo.chair (
     id INTEGER NOT NULL PRIMARY KEY,
@@ -45,4 +45,4 @@ CREATE TABLE isuumo.chair (
 ALTER TABLE
     isuumo.chair
 ADD
-    INDEX `idx_popularity_id`(popularity DESC, id);
+    INDEX `idx_popularity_id`(popularity, id);
