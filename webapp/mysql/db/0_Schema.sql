7db23a318af528e4fa793355c5a0888ede864b30
YumaIshida00 <yu_basket11@icloud.com>
2022-10-15 15:03:13 +0900

    INDEX追加

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -1,38 +1,63 @@
 DROP DATABASE IF EXISTS isuumo;
+
 CREATE DATABASE isuumo;
 
 DROP TABLE IF EXISTS isuumo.estate;
+
 DROP TABLE IF EXISTS isuumo.chair;
 
-CREATE TABLE isuumo.estate
-(
-    id          INTEGER             NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)         NOT NULL,
-    description VARCHAR(4096)       NOT NULL,
-    thumbnail   VARCHAR(128)        NOT NULL,
-    address     VARCHAR(128)        NOT NULL,
-    latitude    DOUBLE PRECISION    NOT NULL,
-    longitude   DOUBLE PRECISION    NOT NULL,
-    rent        INTEGER             NOT NULL,
-    door_height INTEGER             NOT NULL,
-    door_width  INTEGER             NOT NULL,
-    features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+CREATE TABLE isuumo.estate (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    thumbnail VARCHAR(128) NOT NULL,
+    address VARCHAR(128) NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    rent INTEGER NOT NULL,
+    door_height INTEGER NOT NULL,
+    door_width INTEGER NOT NULL,
+    features VARCHAR(64) NOT NULL,
+    popularity INTEGER NOT NULL
 );
 
-CREATE TABLE isuumo.chair
-(
-    id          INTEGER         NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)     NOT NULL,
-    description VARCHAR(4096)   NOT NULL,
-    thumbnail   VARCHAR(128)    NOT NULL,
-    price       INTEGER         NOT NULL,
-    height      INTEGER         NOT NULL,
-    width       INTEGER         NOT NULL,
-    depth       INTEGER         NOT NULL,
-    color       VARCHAR(64)     NOT NULL,
-    features    VARCHAR(64)     NOT NULL,
-    kind        VARCHAR(64)     NOT NULL,
-    popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+ALTER TABLE
+    isuumo.estate
+ADD
+    INDEX `idx_rent`(`rent`);
+
+ALTER TABLE
+    isuumo.estate
+ADD
+    INDEX `idx_doorwidth_doorheight`(`door_width`, `door_height`);
+
+ALTER TABLE
+    isuumo.estate
+ADD
+    INDEX `idx_doorheight`(`door_height`);
+
+ALTER TABLE
+    isuumo.estate
+ADD
+    INDEX `idx_latitude_longitude`(`latitude`, `longitude`);
+
+CREATE TABLE isuumo.chair (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    thumbnail VARCHAR(128) NOT NULL,
+    price INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    width INTEGER NOT NULL,
+    depth INTEGER NOT NULL,
+    color VARCHAR(64) NOT NULL,
+    features VARCHAR(64) NOT NULL,
+    kind VARCHAR(64) NOT NULL,
+    popularity INTEGER NOT NULL,
+    stock INTEGER NOT NULL
 );
+
+ALTER TABLE
+    isuumo.chair
+ADD
+    INDEX `idx_popularity_id`(`popularity`, `id`);
