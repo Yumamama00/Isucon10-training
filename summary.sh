38fbcdf05fa119f0660515f4afbcdc4727bf6c53
YumaIshida00 <yu_basket11@icloud.com>
2022-10-15 16:54:30 +0900

    summary.sh修正

--- summary.sh
+++ summary.sh
@@ -5,9 +5,9 @@
 sudo cat /var/log/nginx/access.log | alp ltsv > /tmp/alp.txt
 # 正規表現でグルーピングしたalp出力
 # 合計降順
-sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/chair/search,/api/chair/low_priced,/api/chair/search/condition,/api/chair/buy/.*,/api/estate/search,/api/estate/low_priced,/api/estate/req_doc/.*,/api/estate/nazotte,/api/estate/search/condition,/api/recommended_estate/.*,/api/chair/.*,/api/estate/.*' --sort sum -r > /tmp/alp-grouped-sum.txt
+sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/recommended_estate/.*,/api/estate/search/condition,/api/estate/nazotte,/api/estate/req_doc/.*,/api/estate/low_priced,/api/estate/search,/api/estate,/api/estate/.*,/api/chair/buy/.*,/api/chair/search/condition,/api/chair/low_priced,/api/chair/search,/api/chair,/api/chair/.*' --sort sum -r > /tmp/alp-grouped-sum.txt
 # 平均降順
-sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/chair/search,/api/chair/low_priced,/api/chair/search/condition,/api/chair/buy/.*,/api/estate/search,/api/estate/low_priced,/api/estate/req_doc/.*,/api/estate/nazotte,/api/estate/search/condition,/api/recommended_estate/.*,/api/chair/.*,/api/estate/.*' --sort avg -r > /tmp/alp-grouped-avg.txt
+sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/recommended_estate/.*,/api/estate/search/condition,/api/estate/nazotte,/api/estate/req_doc/.*,/api/estate/low_priced,/api/estate/search,/api/estate,/api/estate/.*,/api/chair/buy/.*,/api/chair/search/condition,/api/chair/low_priced,/api/chair/search,/api/chair,/api/chair/.*' --sort avg -r > /tmp/alp-grouped-avg.txt
 
 # スロークエリ解析結果を出力
 sudo pt-query-digest /var/log/mysql/mysql-slow.log > /tmp/pt-query-digest.txt
